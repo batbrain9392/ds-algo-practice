224d9e7e06a76afae08057ae4ec866e3dd62219c
batbrain9392 <batbrain9392@gmail.com>
2022-04-28 20:34:32 +0100

    practised merge sort

--- packages/codility/src/index.ts
+++ packages/codility/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/binary-gap';
 export * from './lib/cyclic-rotation';
 export * from './lib/odd-occurrences-in-array';
 export * from './lib/frog-jump';
+export * from './lib/merge-sort';
---
+++ packages/codility/src/lib/merge-sort/index.spec.ts
@@ -0,0 +1,16 @@
+import { mergeSort } from './index';
+
+describe('mergeSort', () => {
+  it('mergeSort([5, 2, 8, 9, 1, 6, 3]) to be [1, 2, 3, 5, 6, 8, 9]', () => {
+    expect(mergeSort([5, 2, 8, 9, 1, 6, 3])).toEqual([1, 2, 3, 5, 6, 8, 9]);
+  });
+  it('mergeSort([9]) to be [9]', () => {
+    expect(mergeSort([9])).toEqual([9]);
+  });
+  it('mergeSort([]) to be []', () => {
+    expect(mergeSort([])).toEqual([]);
+  });
+  it('mergeSort([9, 9, 9, 9, 9]) to be [9, 9, 9, 9, 9]', () => {
+    expect(mergeSort([9, 9, 9, 9, 9])).toEqual([9, 9, 9, 9, 9]);
+  });
+});
