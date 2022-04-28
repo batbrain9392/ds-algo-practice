70321a753f52bc149493efc50741def148d8647f
batbrain9392 <batbrain9392@gmail.com>
2022-04-28 22:08:31 +0100

    export function

--- packages/codility/src/index.ts
+++ packages/codility/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/cyclic-rotation';
 export * from './lib/odd-occurrences-in-array';
 export * from './lib/frog-jump';
 export * from './lib/merge-sort';
+export * from './lib/quick-sort';
