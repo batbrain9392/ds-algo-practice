0724a7dca7954b7028dbcf0d151736bbdf7e1235
batbrain9392 <batbrain9392@gmail.com>
2022-04-28 22:07:08 +0100

    more tests

--- packages/codility/src/lib/merge-sort/index.spec.ts
+++ packages/codility/src/lib/merge-sort/index.spec.ts
@@ -4,6 +4,9 @@ describe('mergeSort', () => {
   it('mergeSort([5, 2, 8, 9, 1, 6, 3]) to be [1, 2, 3, 5, 6, 8, 9]', () => {
     expect(mergeSort([5, 2, 8, 9, 1, 6, 3])).toEqual([1, 2, 3, 5, 6, 8, 9]);
   });
+  it('mergeSort([5, 2, 8, 2]) to be [2, 2, 5, 8]', () => {
+    expect(mergeSort([5, 2, 8, 2])).toEqual([2, 2, 5, 8]);
+  });
   it('mergeSort([9]) to be [9]', () => {
     expect(mergeSort([9])).toEqual([9]);
   });
