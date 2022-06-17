551448bf07638b73e4e45979962f9fe249856d15
batbrain9392 <batbrain9392@gmail.com>
2022-06-17 07:46:56 +0100

    improved algo

--- packages/codility/src/lib/frog-river-one/index.spec.ts
+++ packages/codility/src/lib/frog-river-one/index.spec.ts
@@ -1,8 +1,8 @@
 import { frogRiverOne } from './index';
 
 describe('frogRiverOne', () => {
-  it('frogRiverOne(5, [1, 3, 1, 4, 2, 3, 5, 4]) to be 6', () => {
-    expect(frogRiverOne(5, [1, 3, 1, 4, 2, 3, 5, 4])).toBe(6);
+  it('frogRiverOne(5, [1, 3, 6, 4, 2, 3, 5, 4]) to be 6', () => {
+    expect(frogRiverOne(5, [1, 3, 6, 4, 2, 3, 5, 4])).toBe(6);
   });
   it('frogRiverOne(5, [1, 3, 1, 4, 6, 3, 5, 4]) to be -1', () => {
     expect(frogRiverOne(5, [1, 3, 1, 4, 6, 3, 5, 4])).toBe(-1);
--- packages/codility/src/lib/frog-river-one/index.ts
+++ packages/codility/src/lib/frog-river-one/index.ts
@@ -1,13 +1,9 @@
 export function frogRiverOne(X: number, A: number[]) {
-  const expectedTotal = (X * (X + 1)) / 2;
   const set = new Set<number>();
-  let setTotal = 0;
   for (let i = 0; i < A.length; i++) {
-    if (!set.has(A[i])) {
+    if (A[i] <= X) {
       set.add(A[i]);
-      setTotal += A[i];
-      if (set.size === X && setTotal === expectedTotal) return i;
-      if (set.size > X) break;
+      if (set.size === X) return i;
     }
   }
   return -1;
